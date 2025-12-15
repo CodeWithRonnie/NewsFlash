d791401b071a6bdca89732f606d439585c26fba9
Mamikie Maemu <maemu.ronewa98@gmail.com>
2025-12-15 23:21:11 +0200

    started building my newsflash news site by adding a header name and description

---
+++ newsflash/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header  style={{ border: "2px solid red" }}>
+      <h1>NewsFlash</h1>
+      <p>Your daily dose of the latest news</p>
+    </header>
+  );
+}
+
+export default Header;
